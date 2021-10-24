54dc59371f660a7c9494fb729dbfdb669caa28dd
IvanLuLyf <835498692@qq.com>
2021-10-24 17:59:14 +0800

    Initial Commit

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -2,12 +2,6 @@
 
 use BunnyPHP\Controller;
 
-/**
- * Created by PhpStorm.
- * User: IvanLu
- * Date: 2018/8/12
- * Time: 1:05
- */
 class IndexController extends Controller
 {
     function ac_index()
