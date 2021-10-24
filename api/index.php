e2f13500c4590df45743f5845a3c5b75451cd6a9
IvanLuLyf <835498692@qq.com>
2021-10-24 18:01:05 +0800

    Initial Commit

--- api/index.php
+++ api/index.php
@@ -3,6 +3,11 @@
 use BunnyPHP\BunnyPHP;
 
 header('X-Powered-By:BunnyFramework');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Request-Methods:GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+
 const APP_PATH = __DIR__ . '/../';
 const APP_DEBUG = true;
 date_default_timezone_set('PRC');
